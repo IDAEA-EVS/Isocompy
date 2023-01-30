7bd66ef0e596456afcbbdf9eadef3ce6bb8470a5
IDAEA-EVS <ashgeo@cid.csic.es>
2023-01-30 15:22:20 +0100

    rtd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ author = 'Ashkan Hassanzadeh'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst-parser"]
+extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = []
